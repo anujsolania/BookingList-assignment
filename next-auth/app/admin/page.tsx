e36578bb1645f402985fb00cdf7bca408b6967d0
anuj solania <anujsolania0507@gmail.com>
2025-05-14 18:27:18 +0530

    forms page && prisma-schema

--- next-auth/app/admin/page.tsx
+++ next-auth/app/admin/page.tsx
@@ -1,17 +1,38 @@
 "use client"
 import { useState } from "react"
+import Appbar from "../components/Appbar"
+import { PrismaClient } from "../generated/prisma";
 
 export default function Admin() {
-    const [newfirstname,setnewfirstname] = useState("")
-    const [newlastname,setnewlastname] = useState("")
-    const[newemail,setnewemail] = useState("")
-    const[newpassword,setnewpassword] = useState("")
+    const [name,setname] = useState("")
+    const [address,setaddress] = useState("")
+    const[pin,setpin] = useState("")
+    const[phone,setphone] = useState("")
+
+    const [show,setshow] = useState(false)
 
     // const {setfirstname,setlastname} = useContext(MyContext)
 
     return (
-        // style={{right: showupdateinfo ? "0px" : "-100vw"}} 
-        <div className="h-screen w-screen fixed flex justify-center items-center backdrop-blur-xs transition-all duration-1000 bg-[#161B22]" >
+        <>
+        {/* // style={{right: showupdateinfo ? "0px" : "-100vw"}}  */}
+            <div style={{ padding: "2%"}} className=" bg-[#161B22]" >
+        {/* <Navabar shownav={shownav} setshownav={setshownav} name={name}  ></Navabar> */}
+        <div className="flex w-full" > 
+        <div className="w-[50%] text-2xl font-extrabold" >
+            <h1 className="text-white" >Forms Page</h1>
+        </div>
+        <div className="w-[50%] flex items-center justify-end gap-3" >
+            <button className="border rounded px-4 bg-white"
+            onClick={() => {
+                setshow(true)
+            }} >CREATE FORM</button>
+        </div>
+        </div> 
+    </div>
+
+    {show? 
+           <div className="h-screen w-screen fixed flex justify-center items-center backdrop-blur-xs transition-all duration-1000 bg-[#161B22]" >
             <div className="h-fit w-fit sm:w-90 flex flex-col justify-center items-center border border-black rounded-lg p-6 bg-white/90" >
 
                 <div className="w-full flex " >
@@ -24,16 +45,16 @@ export default function Admin() {
                 <p className="" >Enter the following fields</p>
 
                 <input className="border w-full p-[2%] rounded" type="text" placeholder="Enter firstname"
-                value={newfirstname} onChange={(e) => {setnewfirstname(e.target.value)}} ></input>
+                value={name} onChange={(e) => {setname(e.target.value)}} ></input>
                 
                 <input className="border w-full p-[2%] rounded" type="text" placeholder="Enter address"
-                value={newlastname} onChange={(e) => {setnewlastname(e.target.value)}} ></input>
+                value={address} onChange={(e) => {setaddress(e.target.value)}} ></input>
 
                 <input className="border w-full p-[2%] rounded" type="text" placeholder="Enter PIN"
-                value={newemail} onChange={(e) => {setnewemail(e.target.value)}} ></input>
+                value={pin} onChange={(e) => {setpin(e.target.value)}} ></input>
 
                 <input className="border w-full p-[2%] rounded" type="text" placeholder="Enter Phone number"
-                value={newpassword} onChange={(e) => {setnewpassword(e.target.value)}} ></input>
+                value={phone} onChange={(e) => {setphone(e.target.value)}} ></input>
 
                 <button onClick={async ()=> { 
                 try {
@@ -62,6 +83,9 @@ export default function Admin() {
                 </div>
                 
             </div>
-        </div>
+        </div> : <div className="bg-[#161B22] h-screen w-screen" > 
+            <h1 className="text-5xl text-white text-center" ></h1></div>
+    }
+         </>
     )
 }
